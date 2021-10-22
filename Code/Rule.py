68167202ba75918c9d73db3f50156635b844b1e7
PNWhy5 <83821023+PNWhy5@users.noreply.github.com>
2021-10-22 13:56:41 +0700

    Rule (Text formatted)

--- Code/Rule.py
+++ Code/Rule.py
@@ -48,10 +48,10 @@ def Textpage (page) :
         "90 Development cards\n" \
         "10 Noble tiles"
     
-    EndGame = "\t When a player reaches 15 prestige points," \
+    EndGame = "- \t When a player reaches 15 prestige points," \
         " complete the current round so that each player has played the same number of turns.\n" \
-        " \t The player who then has the highest number of prestige points is declared the winner (don't forget to count your nobles).\n" \
-        " \t In case of a tie, the player who has purchased the fewest development cards wins."
+        "- \t The player who then has the highest number of prestige points is declared the winner (don't forget to count your nobles).\n" \
+        "- \t In case of a tie, the player who has purchased the fewest development cards wins."
     
     Game2Player = "- \t Remove 3 tokens of each gem color \n \t (there should be only 4 of each remaining).\n" \
         "- \t Don't touch the gold.\n" \
@@ -67,51 +67,58 @@ def Textpage (page) :
     
     Setup2 = "- \t The tokens are place in 6 distinct piles (sort them by color) next to the decks at left."
 
-    DevCard = "To win prestige points, the players must purchase development cards.\n" \
-        "These cards are visible in the middle of the table and may be purchased by all players during the game.\n" \
-        "The developments in hand are the cards which the players reserve throughout the game.\n" \
-        "Developments in hand may only be purchased by the players holding them."
+    DevCard = "- \t To win prestige points, the players must purchase development cards.\n" \
+        "- \t These cards are visible in the middle of the table and may be purchased by all players during the game.\n" \
+        "- \t The developments in hand are the cards which the players reserve throughout the game.\n" \
+        "- \t Developments in hand may only be purchased by the players holding them."
 
-    NobleCard = "The noble tiles are visible in the middle of the table.\n" \
-        "At the end of their turn, a player automatically receives the visit from a noble if that player has the amount of bonuses (and only bonuses) required, and they get the corresponding tile.\n" \
-        "A player cannot refuse a visit from a noble.\n" \
-        "Receiving a noble isn't considered to be an action. Each noble tile is worth 3 prestige points, but players can only get a single one per turn."
+    NobleCard = "- \t The noble tiles are visible in the middle of the table.\n" \
+        "- \t At the end of their turn, a player automatically receives the visit from a noble if that player has the amount of bonuses (and only bonuses) required, and they get the corresponding tile.\n" \
+        "- \t A player cannot refuse a visit from a noble.\n" \
+        "- \t Receiving a noble isn't considered to be an action. Each noble tile is worth 3 prestige points, but players can only get a single one per turn."
 
-    Gameplay = "The turn order is chosen randomly at the start.\n" \
+    Gameplay1 = "The turn order is chosen randomly at the start.\n" \
         "On their turn, a player must choose to perform only one of the following four actions:\n" \
         "\t - \t Take 3 gem tokens of different colors.\n" \
-        "\t - \t Take 2 gem tokens of the same color. This action is only possible if there are at least 4 tokens of the chosen color left when the player takes them.\n" \
-        "\t - \tReserve 1 development card and take 1 gold token (joker).\n" \
-        "\t - \tPurchase 1 face-up development card from the middle of the table or a previously reserved one."
+        "\t - \t Take 2 gem tokens of the same color.\n" \
+        "\t This action is only possible if there are at least 4 tokens of the chosen color left when the player takes them."
+    
+    Gameplay2 = "\t - \t Reserve 1 development card and take 1 gold token (joker).\n" \
+        "\t - \t Purchase 1 face-up development card from the middle of the table or a previously reserved one."
 
-    Selecttoken = "A player can never have more than 10 tokens at the end of their turn (including jokers).\n" \
-        "If this happens, they must return tokens until they only have 10 left.\n" \
-        "Note: players may not take 2 tokens of the same color if there are less than 4 tokens available of that color."
+    Selecttoken = "- \t A player can never have more than 10 tokens at the end of their turn (including jokers).\n" \
+        "- \t If this happens, they must return tokens until they only have 10 left.\n \n" \
+        "Note : players may not take 2 tokens of the same color if there are less than 4 tokens available of that color."
 
-    Reserve = "To reserve a card, a player simply needs to take a face-up development from the middle of the table or (if you're feeling lucky) draw the first card from one of the three decks (level 1; level 2 ; level 3) without showing it to the other players.\n" \
-        "The reserved cards are kept in hand and cannot be discarded.\n" \
-        "Players may not have more than three reserved cards in hand, and the only way to get rid of a card is to buy it.\n" \
-        "Reserving a card is also the only way to get a gold token (joker).\n" \
-        "If there is no gold left, you can still reserve a card, but you won't get any gold."
+    Reserve1 = "- \t To reserve a card, a player simply needs to take a face-up development from the middle of the table or (if you're feeling lucky) draw the first card from one of the three decks (level 1; level 2 ; level 3) without showing it to the other players.\n" \
+        "- \t The reserved cards are kept in hand and cannot be discarded.\n" \
+    
+    Reserve2 = "- \t Players may not have more than three reserved cards in hand, and the only way to get rid of a card is to buy it.\n" \
+        "- \t Reserving a card is also the only way to get a gold token (joker).\n" \
+        "- \t If there is no gold left, you can still reserve a card, but you won't get any gold."
 
-    Buying = "To purchase a card, a player must spend the number of tokens indicated on the card.\n"\
-        "A joker token can replace any color.\n" \
-        "The spent tokens (including any jokers) are returned to the middle of the table.\n" \
-        "A player may purchase one of the face-up development cards in the middle of the table or a card in his hand that was reserved on a previous turn.\n" \
-        "Each player makes distinct rows with the acquired development cards by sorting them by color, and staggering them vertically so that their bonuses and prestige point values are visible.\n" \
-        "The bonuses and prestige points granted by each card must be visible to all at all times.\n" \
-        "Note: when a development card from the middle of the table is acquired or reserved, it must immediately be replaced by a card of the same level.\n" \
-        "At all times during the game, there must be 4 face-up cards of each level (unless the deck in question is empty, in which case the empty spaces also remain empty)."
+    Buying1 = "- \t To purchase a card, a player must spend the number of tokens indicated on the card.\n"\
+        "- \t A joker token can replace any color.\n" \
+        "- \t The spent tokens (including any jokers) are returned to the middle of the table.\n" \
+        "- \t A player may purchase one of the face-up development cards in the middle of the table or a card in his hand that was reserved on a previous turn.\n" \
 
-    Bonus = "The bonuses a player has from development cards acquired on previous turns provide discounts on the purchase of new cards.\n" \
-        "Each bonus of a given color is equal to a token of that color.\n" \
-        "Thus, if a player has 2 blue bonuses and wants to purchase a card which costs 2 blue tokens and 1 green token, the player must only spend 1 green token.\n" \
-        "If a player has enough development cards (and therefore bonuses), they can even purchase a card without spending any tokens."
+    Buying2 = "- \t Each player makes distinct rows with the acquired development cards by sorting them by color, and staggering them vertically so that their bonuses and prestige point values are visible.\n" \
+        "- \t The bonuses and prestige points granted by each card must be visible to all at all times.\n" \
 
-    Noble = "At the end of their turn, each player checks the noble tiles in order to determine if they're receiving a visit from one of them. A player can be visited if they have (at least) the quantity and type of bonuses indicated on the noble tile.\n" \
-        "It is impossible to refuse the visit from a noble, which is not considered to be an action.\n" \
-        "If a player has enough bonuses to be visited by more than one noble at the end of their turn, that player chooses the noble to be received.\n" \
-        "The tile obtained is placed face-up in front of the player in question."
+    Buying3 =  "Note: when a development card from the middle of the table is acquired or reserved, it must immediately be replaced by a card of the same level.\n" \
+    "\t At all times during the game, there must be 4 face-up cards of each level (unless the deck in question is empty, in which case the empty spaces also remain empty)."
+
+    Bonus1 = "- \t The bonuses a player has from development cards acquired on previous turns provide discounts on the purchase of new cards.\n" \
+        "- \t Each bonus of a given color is equal to a token of that color.\n" \
+        "- \t Thus, if a player has 2 blue bonuses and wants to purchase a card which costs 2 blue tokens and 1 green token, the player must only spend 1 green token.\n" \
+    
+    Bonus2 = "- \t If a player has enough development cards (and therefore bonuses), they can even purchase a card without spending any tokens."
+
+    Noble1 = "- \t At the end of their turn, each player checks the noble tiles in order to determine if they're receiving a visit from one of them. A player can be visited if they have (at least) the quantity and type of bonuses indicated on the noble tile.\n" \
+        "- \t It is impossible to refuse the visit from a noble, which is not considered to be an action.\n" \
+
+    Noble2 = "- \t If a player has enough bonuses to be visited by more than one noble at the end of their turn, that player chooses the noble to be received.\n" \
+        "- \t The tile obtained is placed face-up in front of the player in question."
 
     #return text for the page
     if page == 1 :
@@ -129,19 +136,32 @@ def Textpage (page) :
     if page == 7 :
         return NobleCard
     if page == 8 :
-        return Gameplay
+        return Gameplay1
     if page == 9 :
-        return Selecttoken
+        return Gameplay2
     if page == 10 :
-        return Reserve
+        return Selecttoken
     if page == 11 :
-        return Buying
+        return Reserve1
     if page == 12 :
-        return Bonus
+        return Reserve2
     if page == 13 :
-        return Noble
+        return Buying1
     if page == 14 :
+        return Buying2
+    if page == 15 :
+        return Buying3
+    if page == 16 :
+        return Bonus1
+    if page == 17 :
+        return Bonus2
+    if page == 18 :
+        return Noble1
+    if page == 19 :
+        return Noble2
+    if page == 20 :
         return EndGame
+    
 
 def ruletext (page,text_font_bold,text_font_regular) :
     #Components Text render 
@@ -172,32 +192,56 @@ def ruletext (page,text_font_bold,text_font_regular) :
         if page == 7 :
             text_block(text_font_bold , "The Noble Tiles", 'Black', (520,150), 1030, screen, 10)
             text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
-        #Game Play
+        #Game Play 1 / 2
         if page == 8 :
-            text_block(text_font_bold , "Game Play", 'Black', (550,150), 1030, screen, 10)
+            text_block(text_font_bold , "Game Play 1/2", 'Black', (550,150), 1030, screen, 10)
+            text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
+        #Game Play 2 / 2
+        if page == 9 :
+            text_block(text_font_bold , "Game Play 2/2", 'Black', (550,150), 1030, screen, 10)
             text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
         #Selecting Tokens
-        if page == 9 :
+        if page == 10 :
             text_block(text_font_bold , "Selecting Tokens", 'Black', (480,150), 1030, screen, 10)
             text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
-        #Reserve a development card
-        if page == 10 :
-            text_block(text_font_bold , "Reserve a development card", 'Black', (370,150), 1030, screen, 10)
-            text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
-        #Buying a development card
+        #Reserve a development card 1 / 2
         if page == 11 :
-            text_block(text_font_bold , "Buying a development card", 'Black', (370,150), 1030, screen, 10)
+            text_block(text_font_bold , "Reserve a development card 1/2", 'Black', (370,150), 1030, screen, 10)
             text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
-        #The Bonuses
+        #Reserve a development card 2 / 2
         if page == 12 :
-            text_block(text_font_bold , "The Bonuses", 'Black', (550,150), 1030, screen, 10)
+            text_block(text_font_bold , "Reserve a development card 2/2", 'Black', (370,150), 1030, screen, 10)
             text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
-        #The Nobles
+        #Buying a development card 1 / 3
         if page == 13 :
-            text_block(text_font_bold , "The Nobles", 'Black', (550,150), 1030, screen, 10)
+            text_block(text_font_bold , "Buying a development card 1/3", 'Black', (370,150), 1030, screen, 10)
+            text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
+        #Buying a development card 2 / 3
+        if page == 14 :
+            text_block(text_font_bold , "Buying a development card 2/3", 'Black', (370,150), 1030, screen, 10)
+            text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
+        #Buying a development card 3 / 3
+        if page == 15 :
+            text_block(text_font_bold , "Buying a development card 3/3", 'Black', (370,150), 1030, screen, 10)
+            text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
+        #The Bonuses 1 / 2
+        if page == 16 :
+            text_block(text_font_bold , "The Bonuses 1/2", 'Black', (550,150), 1030, screen, 10)
+            text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
+        #The Bonuses 2 / 2
+        if page == 17 :
+            text_block(text_font_bold , "The Bonuses 2/2", 'Black', (550,150), 1030, screen, 10)
+            text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
+        #The Nobles 1 / 2
+        if page == 18 :
+            text_block(text_font_bold , "The Nobles 1/2", 'Black', (550,150), 1030, screen, 10)
+            text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
+        #The Nobles 2 / 2
+        if page == 19 :
+            text_block(text_font_bold , "The Nobles 2/2", 'Black', (550,150), 1030, screen, 10)
             text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
         #End of the Game 
-        if page == 14 :
+        if page == 20 :
             text_block(text_font_bold , "End of the Game", 'Black', (480,150), 1030, screen, 10)
             text_block(text_font_regular, Textpage (page), 'Black', (270,230), 1030, screen, 15)
 
@@ -222,7 +266,7 @@ def rulebook(screen, FPS) :
     Back_rect = Back_surface.get_rect(topleft = (1150,640))
     #Set page of text. Default is page 1
     page = 1
-    allpage = 14
+    allpage = 20
 
     #Game loop
     run = True
@@ -269,7 +313,7 @@ def rulebook(screen, FPS) :
         screen.blit(BACKGROUND,(0,0))
         #Text page set up and render
         Page_surface = text_font_regular.render('Page '+str(page)+'/'+str(allpage),True,'White')
-        screen.blit(Page_surface,(30,640))
+        screen.blit(Page_surface,(25,640))
         #Setting Button Text in loop because they need to change color when hover
         Next_surface = text_font_regular.render('Next',True,Color[0]).convert_alpha()
         Prev_surface = text_font_regular.render('Previous',True,Color[1]).convert_alpha()
